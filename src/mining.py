2456ebfeeeff091b827708e2e38d6c98d2052863
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-03-02 17:25:41 +0000

    Minimal scatter plot view (kmeans)

--- src/mining.py
+++ src/mining.py
@@ -1,23 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.cross_validation import cross_val_score
+import numpy as np
+import pylab as pl
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import KMeans
+from sklearn import datasets
 
 import config
 
 # Parsing raw CSV file
 db = pd.read_csv(config.db_path)
-#print db.columns
+print db.columns
 
-et = ExtraTreesClassifier(n_estimators=100, max_depth=None, min_samples_split=1, random_state=0)
+centers = [[1, 1], [-1, -1], [1, -1]]
+iris = datasets.load_iris()
 
-columns = ["latitude", "longitude"]
+X = db.filter(['latitude', 'longitude', 'hour_taken']).values[:100]
+y = db['row ID'].values[:100]
 
-sample = 3000
-labels = db["hour_taken"].values[:sample]
-features = db[list(columns)].values[:sample]
+print iris.data[:10]
+print X[:10]
 
-et_score = cross_val_score(et, features, labels, n_jobs=-1).mean()
+estimators = {'k_means_4': KMeans(n_clusters=4),
+              'k_means_8': KMeans(n_clusters=8)}
 
-print "{0} -> ET: {1})".format(columns, et_score)
+for fignum, (name, estimator) in enumerate(estimators.iteritems()):
+    fig = pl.figure(fignum, figsize=(4, 3))
+    pl.clf()
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+    pl.cla()
+    estimator.fit(X)
+    labels = estimator.labels_
+
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=labels.astype(np.float))
+
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+
+    ax.set_xlabel('Latitude')
+    ax.set_ylabel('Longitude')
+    ax.set_zlabel('Hour taken')
+
+# Plot the ground truth
+fig = pl.figure(fignum, figsize=(4, 3))
+pl.clf()
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+pl.cla()
+pl.show()
