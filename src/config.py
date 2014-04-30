691c9a3c7dd225fbc09f04009e2205cbd73834f8
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-04-29 21:38:02 -0700

    Delete config.py

--- src/config.py
+++
@@ -1,8 +0,0 @@
-db_path = '../sujet/flickr.csv'
-google_api_key = 'AIzaSyDmuqQyTSBFtwzGtmiHV3mckRZUUVxC9fA'
-
-flickr_api_key = 'e7e80b3317973ea8d7edb1a3e6a99347'
-flickr_api_secret = '7a9cdb5ac7995ca8'
-
-db_df_pickle = 'flickr.pickle'
-cluster_df_pickle = 'cluster.pickle'
